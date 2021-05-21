e4569e7fa44fe78152b80da1bb3cf78029ee49ae
kielx <kiel66@gmail.com>
2021-05-21 19:23:54 +0200

    Add typewriter effect and styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,21 +25,17 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
+          margin: `2rem auto`,
+          maxWidth: `80%`,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
+        <footer>
+          © {new Date().getFullYear()} Krzysztof Pantak, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import Typewriter from "typewriter-effect"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,18 +9,27 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h1 style={{ marginBottom: 0 }}>Krzysztof_Pantak@_Dev:</h1>
+    <div style={{ display: "inline-flex" }}>
+      <h1>$:</h1>
+      <h1>
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+            strings: [
+              "Software Developer",
+              "MERN Stack Developer",
+              "Computer Science Student",
+            ],
+            deleteSpeed: "natural",
+          }}
+        />
+      </h1>
+    </div>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ display: "inline-flex", width: 500 }}>
-      <StaticImage
-        src="../images/testmojephoto2.png"
-        quality={100}
-        width={300}
-        alt="My photo"
-        style={{ marginBottom: `1.45rem` }}
-      />
-
       <p style={{ width: "50%", display: "inline-block" }}>
         <Link to="/page-2/">Go to page 2</Link> <br />
         <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
