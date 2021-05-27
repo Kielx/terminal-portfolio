568cd9038090cd6de416358114ce3c2e57b1309d
kielx <kiel66@gmail.com>
2021-05-27 21:37:41 +0200

    Rework terminal windows to be rendered from list of items dynamically

---
+++ src/components/ItemsList.js
@@ -0,0 +1,68 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//importing winbox https://github.com/nextapps-de/winbox/issues/1
+import WinBox from "winbox/src/js/winbox"
+import "winbox/dist/css/winbox.min.css"
+
+import PopupTerminalWindow from "../components/PopupTerminalWindow"
+
+export default function ItemsList() {
+  const myItems = [
+    {
+      listName: "/About",
+      popupHeader: "About me",
+      popupImageSrc: "mojephotoASCII.png",
+      popupImageAlt: "Photo of myself rendered with ASCII characters",
+      popupText:
+        "Software Developer currently working on finishing my CS Degree while also being a Law Enforcer, EULEX Peacekeeping Mission in Kosovo Veteran, Strength Coach and a family man. Based in 🇵🇱 Poland.",
+    },
+    {
+      listName: "/📈 Expenses Analyzer",
+      popupHeader: "Expenses Analyzer",
+      popupImageSrc: "expenses-analyzer-ascii.png",
+      popupImageAlt: "Expenses analyzer ASCII icon",
+      popupText:
+        "Expenses analyzer is a React-based app, that allows the user to analyze bank statements from (at the moment) polish Mbank bank accounts.",
+    },
+  ]
+
+  const checkScreenWidth = () => {
+    return window.screen.width > 1000 ? "60%" : "100%"
+  }
+
+  const listItems = myItems.map(item => (
+    <li>
+      <a
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          const win = new WinBox({
+            title: item.popupHeader,
+            width: checkScreenWidth(),
+            height: checkScreenWidth(),
+            x: "center",
+            y: "center",
+            onfocus: function () {
+              this.setBackground("#00aa00")
+            },
+            onblur: function () {
+              this.setBackground("#777")
+            },
+          })
+          ReactDOM.render(
+            React.createElement(PopupTerminalWindow, {
+              popupHeader: item.popupHeader,
+              popupImageSrc: item.popupImageSrc,
+              popupImageAlt: item.popupImageAlt,
+              popupText: item.popupText,
+            }),
+            win.body
+          )
+        }}
+      >
+        {item.listName}
+      </a>
+    </li>
+  ))
+
+  return <ul>{listItems}</ul>
+}
--- src/components/LsDisplay.js
+++ src/components/LsDisplay.js
@@ -1,139 +1,20 @@
 import React, { useState } from "react"
-import ReactDOM from "react-dom"
 import Typewriter from "typewriter-effect"
 
-//importing winbox https://github.com/nextapps-de/winbox/issues/1
-import WinBox from "winbox/src/js/winbox"
-import "winbox/dist/css/winbox.min.css"
-import PopupTerminalWindow from "../components/PopupTerminalWindow"
+import ItemsList from "../components/ItemsList"
 import "./styles.css"
 
 const LsDisplay = () => {
-  const [typeWriterCursor, setTypeWriterCursor] = useState("|")
-  const [items, setItems] = useState("")
-
-  const checkScreenWidth = () => {
-    return window.screen.width > 1000 ? "60%" : "100%"
-  }
-
-  const itemsList = (
-    <ul style={{ lineHeight: 1.8 }}>
-      <li>
-        <a
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            new WinBox({
-              title: "About Me",
-              modal: false,
-              x: "20%",
-              y: "20%",
-              onfocus: function () {
-                this.setBackground("#00aa00")
-              },
-              onblur: function () {
-                this.setBackground("#777")
-              },
-              url: "/about",
-            })
-          }}
-        >
-          /About
-        </a>
-      </li>
-      <li style={{ marginLeft: 10, color: "#ccc" }}>
-        ↵ Projects
-        <ul style={{ marginLeft: 50 }}>
-          <a
-            style={{ cursor: "pointer" }}
-            onClick={() => {
-              new WinBox({
-                title: "Expenses Analyzer",
-                modal: false,
-                width: checkScreenWidth(),
-                height: checkScreenWidth(),
-                x: "center",
-                y: "center",
-
-                onfocus: function () {
-                  this.setBackground("#00aa00")
-                },
-                onblur: function () {
-                  this.setBackground("#777")
-                },
-                url: "/expenses-analyzer",
-              })
-            }}
-          >
-            - 📈 Expenses analyzer
-          </a>
-        </ul>
-      </li>
-      <li>
-        <a
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            new WinBox({
-              title: "404",
-              modal: false,
-              x: "20%",
-              y: "20%",
-              onfocus: function () {
-                this.setBackground("#00aa00")
-              },
-              onblur: function () {
-                this.setBackground("#777")
-              },
-              url: "/404",
-            })
-          }}
-        >
-          /404
-        </a>
-      </li>
-      <li>
-        <a
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            const win = new WinBox({
-              width: checkScreenWidth(),
-              height: checkScreenWidth(),
-              x: "center",
-              y: "center",
-              modal: true,
-              onfocus: function () {
-                this.setBackground("#00aa00")
-              },
-              onblur: function () {
-                this.setBackground("#777")
-              },
-            })
-            ReactDOM.render(
-              React.createElement(PopupTerminalWindow, {
-                popupHeader: "About me",
-                popupImageSrc: "mojephotoASCII.png",
-                popupImageAlt: "Photo of myself rendered with ASCII characters",
-                popupText:
-                  "Software Developer currently working on finishing my CS Degree while also being a Law Enforcer, EULEX Peacekeeping Mission in Kosovo Veteran, Strength Coach and a family man. Based in 🇵🇱 Poland.",
-              }),
-              win.body
-            )
-          }}
-        >
-          /About
-        </a>
-      </li>
-    </ul>
-  )
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const TypeWriter = (
     <Typewriter
-      options={{ cursor: typeWriterCursor }}
       onInit={typewriter => {
         typewriter
           .typeString("dir")
           .pauseFor(100)
           .callFunction(() => {
-            setItems(itemsList)
+            setIsLoaded(true)
           })
           .callFunction(function (state) {
             state.elements.cursor.style.display = "none"
@@ -166,7 +47,7 @@ const LsDisplay = () => {
         {"> "}
         {TypeWriter}
       </h3>
-      <p style={{ padding: 5, margin: 0 }}>{items}</p>
+      {isLoaded ? <ItemsList></ItemsList> : ""}
     </div>
   )
 }
