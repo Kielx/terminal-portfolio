603a565092e450cc37aa6c372b4b668f47f56bd6
kielx <kiel66@gmail.com>
2021-06-01 21:32:57 +0200

    Change metada

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kielx Portfolio`,
+    description: `Kielx - Krzysztof Pantak, Software Developer portfolio site.`,
+    author: `@Krzysztof Pantak`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
