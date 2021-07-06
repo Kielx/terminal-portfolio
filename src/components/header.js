5106f9a56752663304fb4f2e58b780b3217532ec
kielx <kiel66@gmail.com>
2021-07-06 19:59:42 +0200

    Make header run only once without looping

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,38 @@
-import * as React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import Typewriter from "typewriter-effect"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div style={{ display: "inline-flex" }}>
-      <h1>{">"}</h1>
-      <h1>
-        <Typewriter
-          style={{ marginTop: 0, paddingTop: 0 }}
-          options={{
-            loop: true,
-            deleteSpeed: "natural",
-          }}
-          onInit={typewriter => {
-            typewriter
-              .typeString("Software Developer")
-              .pauseFor(800)
-              .deleteAll()
-              .typeString("Problem solver")
-              .pauseFor(800)
-              .deleteAll()
-              .typeString("Creator")
-              .pauseFor(800)
-              .deleteAll()
-              .typeString("Krzysztof Pantak")
-              .pauseFor(2500)
-              .deleteAll()
-              .start()
-          }}
-        />
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  return (
+    <header>
+      <div style={{ display: "inline-flex" }}>
+        <h1>{">"}</h1>
+        <h1>
+          <Typewriter
+            style={{ marginTop: 0, paddingTop: 0 }}
+            options={{
+              deleteSpeed: "natural",
+            }}
+            onInit={typewriter => {
+              typewriter
+                .typeString("Software Developer")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Problem solver")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Krzysztof Pantak")
+                .callFunction(function (state) {
+                  state.elements.cursor.style.display = "none"
+                })
+                .start()
+            }}
+          />
+        </h1>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
