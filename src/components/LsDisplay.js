1ee483535303e57623c939e02393af3c87002da0
kielx <kiel66@gmail.com>
2021-07-06 23:04:07 +0200

    Remove typewriter from inside main console for better UX

--- src/components/LsDisplay.js
+++ src/components/LsDisplay.js
@@ -1,41 +1,18 @@
 import React, { useState } from "react"
-import Typewriter from "typewriter-effect"
-
 import ItemsList from "../components/ItemsList"
 import "../styles/styles.scss"
 import "../styles/winbox.scss"
 import "../styles/mainTerminalWindow.scss"
 
 const LsDisplay = ({ text, name, delay }) => {
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  const TypeWriter = (
-    <Typewriter
-      options={{ delay: delay }}
-      onInit={typewriter => {
-        typewriter
-          .typeString(text)
-          .callFunction(() => {
-            setIsLoaded(true)
-          })
-          .callFunction(function (state) {
-            state.elements.cursor.style.display = "none"
-          })
-          .start()
-      }}
-    />
-  )
-
   return (
     <div className="LsDisplayWindow">
       <div className="LsDisplayTaskbar">Cmd</div>
       <h3>
-        {name ? "C:/Users/Krzysztof Pantak" : ""}
+        {name ? "~ $ls -la" : ""}
         <div className="break"></div>
-        {"> "}
-        {TypeWriter}
       </h3>
-      {isLoaded ? <ItemsList></ItemsList> : ""}
+      <ItemsList></ItemsList>
     </div>
   )
 }
