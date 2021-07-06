01214511f10441b3e0021ec6fbf8d1e3dfbc244d
kielx <kiel66@gmail.com>
2021-07-06 22:36:16 +0200

    Fix naming inconsistencies

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import PopupTerminalWindow from "../components/PopupTerminalWindow"
+import "../styles/styles.scss"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
