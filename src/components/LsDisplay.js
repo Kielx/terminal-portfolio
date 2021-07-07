9fc68d0f3efa2bea2b8116f2d5ce215328e49ad9
kielx <kiel66@gmail.com>
2021-07-07 20:43:49 +0200

    test2

--- src/components/LsDisplay.js
+++ src/components/LsDisplay.js
@@ -5,14 +5,23 @@ import "../styles/winbox.scss"
 import "../styles/mainTerminalWindow.scss"
 
 const LsDisplay = ({ text }) => {
+  const checkscreenWidthMobile = () => {
+    if (typeof window !== "undefined") {
+      return window.screen.width < 1024 ? true : false
+    }
+  }
+
   return (
     <div className="LsDisplayWindow">
       <div className="LsDisplayTaskbar">Cmd</div>
       <h2 style={{ color: "inherit", fontSize: "1.1rem", paddingLeft: "4vw" }}>
         {text}
       </h2>
-
-      <ItemsListMobile></ItemsListMobile>
+      {checkscreenWidthMobile() ? (
+        <ItemsListMobile></ItemsListMobile>
+      ) : (
+        <ItemsList></ItemsList>
+      )}
     </div>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,43 +3,32 @@ import PropTypes from "prop-types"
 import Typewriter from "typewriter-effect"
 
 const Header = ({ siteTitle }) => {
-  const [isLoaded, setIsLoaded] = useState(
-    sessionStorage.getItem("isLoaded") || false
-  )
-
-  useEffect(() => {
-    sessionStorage.setItem("isLoaded", isLoaded)
-  }, [isLoaded])
-
   return (
     <header>
       <div style={{ display: "inline-flex" }}>
         <h1>{">"}</h1>
         <h1>
-          {isLoaded ? (
-            "Krzysztof Pantak"
-          ) : (
-            <Typewriter
-              style={{ marginTop: 0, paddingTop: 0 }}
-              options={{
-                deleteSpeed: "natural",
-              }}
-              onInit={typewriter => {
-                typewriter
-                  .typeString("Software Developer")
-                  .pauseFor(2500)
-                  .deleteAll()
-                  .typeString("Problem solver")
-                  .pauseFor(2500)
-                  .deleteAll()
-                  .typeString("Krzysztof Pantak")
-                  .callFunction(() => {
-                    setIsLoaded(true)
-                  })
-                  .start()
-              }}
-            />
-          )}
+          <Typewriter
+            style={{ marginTop: 0, paddingTop: 0 }}
+            options={{
+              deleteSpeed: "natural",
+            }}
+            onInit={typewriter => {
+              typewriter
+                .typeString("Software Developer")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Problem solver")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Krzysztof Pantak")
+                .callFunction(() => {
+                  setIsLoaded(true)
+                })
+                .start()
+            }}
+          />
+          )
         </h1>
       </div>
     </header>
