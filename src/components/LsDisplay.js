55a6383f81d0cd1809d413d7e221d8c9ede27752
kielx <kiel66@gmail.com>
2021-07-07 20:38:58 +0200

    Test

--- src/components/LsDisplay.js
+++ src/components/LsDisplay.js
@@ -5,23 +5,14 @@ import "../styles/winbox.scss"
 import "../styles/mainTerminalWindow.scss"
 
 const LsDisplay = ({ text }) => {
-  const checkscreenWidthMobile = () => {
-    if (typeof window !== "undefined") {
-      return window.screen.width < 1024 ? true : false
-    }
-  }
-
   return (
     <div className="LsDisplayWindow">
       <div className="LsDisplayTaskbar">Cmd</div>
       <h2 style={{ color: "inherit", fontSize: "1.1rem", paddingLeft: "4vw" }}>
         {text}
       </h2>
-      {checkscreenWidthMobile() ? (
-        <ItemsListMobile></ItemsListMobile>
-      ) : (
-        <ItemsList></ItemsList>
-      )}
+
+      <ItemsListMobile></ItemsListMobile>
     </div>
   )
 }
