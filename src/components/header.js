29a33dbbd9399dab52dce2f17fc7abad952702bc
kielx <kiel66@gmail.com>
2021-07-07 12:20:58 +0200

    Single typewriter that stops effect based on sessionStorage to prevent multiple renders of typerwiter effect on each page reload

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import Typewriter from "typewriter-effect"
 
 const Header = ({ siteTitle }) => {
+  const [isLoaded, setIsLoaded] = useState(
+    sessionStorage.getItem("isLoaded") || false
+  )
+
+  useEffect(() => {
+    sessionStorage.setItem("isLoaded", isLoaded)
+  }, [isLoaded])
+
   return (
     <header>
       <div style={{ display: "inline-flex" }}>
         <h1>{">"}</h1>
         <h1>
-          <Typewriter
-            style={{ marginTop: 0, paddingTop: 0 }}
-            options={{
-              deleteSpeed: "natural",
-            }}
-            onInit={typewriter => {
-              typewriter
-                .typeString("Software Developer")
-                .pauseFor(2500)
-                .deleteAll()
-                .typeString("Problem solver")
-                .pauseFor(2500)
-                .deleteAll()
-                .typeString("Krzysztof Pantak")
-                .callFunction(function (state) {
-                  state.elements.cursor.style.display = "none"
-                })
-                .start()
-            }}
-          />
+          {isLoaded ? (
+            "Krzysztof Pantak"
+          ) : (
+            <Typewriter
+              style={{ marginTop: 0, paddingTop: 0 }}
+              options={{
+                deleteSpeed: "natural",
+              }}
+              onInit={typewriter => {
+                typewriter
+                  .typeString("Software Developer")
+                  .pauseFor(2500)
+                  .deleteAll()
+                  .typeString("Problem solver")
+                  .pauseFor(2500)
+                  .deleteAll()
+                  .typeString("Krzysztof Pantak")
+                  .callFunction(() => {
+                    setIsLoaded(true)
+                  })
+                  .start()
+              }}
+            />
+          )}
         </h1>
       </div>
     </header>
