1261dea9d43eb1568d886905ff1e47aaaf554c66
Kielx <kiel66@gmail.com>
2021-08-12 13:48:08 +0200

    Update SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,11 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Chris Pantak Portfolio`,
-    description: `Chris Pantak Software Developer portfolio site. Terminal - command line themed portfolio featuring projects ranging from robust Gatsby PWA's, React APPsand FullStack Web APPs utilizing cloud platforms to simple HTML, CSS and JavaScript sites.`,
-    author: `@Chris Pantak`,
+    description: `Chris Pantak Software Developer portfolio site. Terminal -command-line themed portfolio featuring projects ranging from robust Gatsby PWA's, React APPsand FullStack Web APPs utilizing cloud platforms to simple HTML, CSS and JavaScript sites.`,
+    author: `@ChrisPantak`,
+    url: "https://pantak.net",
+    image: "/PortfolioSS-sm.png", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@ChrisPantak",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
