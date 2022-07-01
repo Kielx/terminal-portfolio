3f53404cf640f44cfa2d53377498b99fdc218c52
kielx <kiel66@gmail.com>
2022-07-01 22:14:06 +0200

    Add description texts as in true ls-l display

--- src/components/LsDisplay.js
+++ src/components/LsDisplay.js
@@ -17,6 +17,7 @@ const LsDisplay = ({ text }) => {
       </div>
       <h2>
         {text}
+        {window.innerWidth > 1024 ? "a" : ""}
         <div className="break"></div>
       </h2>
       {isBrowser ? <ItemsList></ItemsList> : ""}
